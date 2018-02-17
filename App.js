9e9efbef1a5d192cfa5b9d81a4773edbbb8020b7
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-02-17 13:41:21 -0500

    basic navigation

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { DrawerNavigator } from 'react-navigation';
+import { TabNavigator } from 'react-navigation';
+import Explore from './components/Explore.js';
+import Chat from './components/Chat.js';
+import Profile from './components/Profile.js';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   render() {
+    const MainNavigator = TabNavigator({
+      Profile: { screen: Profile},
+      Chat:    { screen: Chat},
+      Explore: { screen: Explore}
+    });
     return (
-      <View style={styles.container}>
-        <Text>Welcome to ClassPass!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+//      <View style={styles.container}>
+//        <Text>Welcome to ClassPass!</Text>
+//        <Text>Changes you make will automatically reload.</Text>
+//        <Text>Shake your phone to open the developer menu.</Text>
+//      </View>
+        <MainNavigator />  
     );
   }
 }
