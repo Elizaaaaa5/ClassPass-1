c3b0df9bab2dbac55cfe748b87daa54f83adb1c4
tay1es <jason.thomas199@gmail.com>
2018-02-17 15:17:55 -0500

    test

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to ClassPass!</Text>
+        <Text>Welcome to ClassPass! Jas</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
---
+++ components/Profile.js
@@ -0,0 +1,1 @@
+<Text>Welcome to the Profile Tab!</Text>
