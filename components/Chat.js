62061673a17a35b6b581fa197154ad8c66331775
tay1es <jason.thomas199@gmail.com>
2018-02-17 17:22:38 -0500

    update chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,1 +1,28 @@
-<Text>Welcome to the Profile Tab!</Text>
+import { List, ListItem } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  ... // more items
+]
+
+<List containerStyle={{marginBottom: 20}}>
+  {
+    list.map((l, i) => (
+      <ListItem
+        roundAvatar
+        avatar={{uri:l.avatar_url}}
+        key={i}
+        title={l.name}
+      />
+    ))
+  }
+</List>
