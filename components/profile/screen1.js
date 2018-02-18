e0c68ece1fbd6b97a5d6781d4264be88c8d2395c
Luyao Chen <elizaclu5@gmail.com>
2018-02-18 03:07:40 -0500

    add profile icon on tab

--- components/profile/screen1.js
+++ components/profile/screen1.js
@@ -1,160 +1,41 @@
-
 import React, { Component } from 'react';
-import {
-  StyleSheet, Text, View, Image, ScrollView, Dimensions, TouchableOpacity, StatusBar
-} from 'react-native';
-import { Button } from 'react-native-elements'
-import { Font } from 'expo';
-import { LinearGradient } from 'expo';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import Explore from './components/Explore.js';
+import Chat from './components/Chat.js';
+import Profile from './components/Profile.js';
+import { Icon } from 'react-native-elements';
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-const IMAGE_SIZE = SCREEN_WIDTH - 80;
-
-class CustomButton extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      selected: false
-    };
-  }
-
-  componentDidMount() {
-    const { selected } = this.props;
-
-    this.setState({
-      selected
-    });
-  }
-
+export default class App extends Component {
   render() {
-    const { title } = this.props;
-    const { selected } = this.state;
-
-    return (
-      <Button
-        text={title}
-        textStyle={{ fontSize: 15, color: 'white', fontFamily: 'regular' }}
-        buttonStyle={selected ? { backgroundColor: '#6c6c6c', borderRadius: 100, width: 127 } : { borderWidth: 1, borderColor: 'white', borderRadius: 30, width: 127, backgroundColor: 'transparent' }}
-        containerStyle={{ marginRight: 10 }}
-        onPress={() => this.setState({ selected: !selected })}
-      />
-    );
-  }
-}
-
-export default class LoginScreen1 extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fontLoaded: false,
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'georgia': require('../assets/fonts/Georgia.ttf'),
-      'regular': require('../assets/fonts/Montserrat-Regular.ttf'),
-      'light': require('../assets/fonts/Montserrat-Light.ttf'),
-      'bold': require('../assets/fonts/Montserrat-Bold.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <StatusBar
-          barStyle="light-content"
-        />
-        { this.state.fontLoaded ?
-          <View style={{flex: 1, backgroundColor: 'black'}}>
-
-            <View style={styles.statusBar} />
-            <View style={styles.navBar}>
-              <Text style={styles.nameHeader}>
-                Profile
-              </Text>
-            </View>
-            <ScrollView style={{flex: 1}}>
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Image
-                  source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg/1200px-South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg' }}
-                  style={{ width: IMAGE_SIZE, height: IMAGE_SIZE, borderRadius: 10}}
-                />
-              </View>
-              <View style={{flex: 1, flexDirection: 'row', marginTop: 20, marginHorizontal: 40, justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{flex: 1, fontSize: 26, color: 'white', fontFamily: 'bold'}}>
-                  Luyao
-                </Text>
-              </View>
-              <View style={{flex: 1, marginTop: 20, width: SCREEN_WIDTH - 80, marginLeft: 40}}>
-                <Text style={{flex: 1, fontSize: 15, color: 'white', fontFamily: 'regular'}}>
-                  Studies Computer Science at the University of Virginia.{"\n"}
-                  Class of 2019.{"\n"}
-                  GPA: 0.0
-                </Text>
-              </View>
-              <View style={{flex: 1, marginTop: 30}}>
-                <Text style={{flex: 1, fontSize: 15, color: '#ffffff', fontFamily: 'regular', marginLeft: 40}}>
-                  TUTOR COURSES
-                </Text>
-                <View style={{flex: 1, width: SCREEN_WIDTH, marginTop: 20}}>
-                  <ScrollView
-                    style={{flex: 1}}
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                  >
-                    <View style={{flex: 1, flexDirection: 'column', height: 170, marginLeft: 40, marginRight: 10}}>
-                      <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Button title="CS2150" buttonStyle={{
-                            backgroundColor: "#1a1a1c"}} />
-                        <Button title="CS4753" buttonStyle={{
-                            backgroundColor: "#1a1a1c"}}/>
-                      </View>
-                    </View>
-                  </ScrollView>
-                </View>
-              </View>
-            </ScrollView>
-          </View> :
-          <Text>Loading...</Text>
+    const MainNavigator = TabNavigator({
+      Explore: { screen: Explore,},
+      Chat:    { screen: Chat},
+      Profile: { screen: Profile}
+    }, {
+      tabBarOptions: {
+        style: {
+          backgroundColor: '#1a1a1c'
         }
-      </View>
+      }
+    });
+    return (
+//      <View style={styles.container}>
+//        <Text>Welcome to ClassPass!</Text>
+//        <Text>Changes you make will automatically reload.</Text>
+//        <Text>Shake your phone to open the developer menu.</Text>
+//      </View>
+        <MainNavigator />
     );
   }
 }
+
 const styles = StyleSheet.create({
-  statusBar: {
-    height: 10,
-  },
-  navBar: {
-    height: 60,
-    width: SCREEN_WIDTH,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignContent: 'center'
   },
-  nameHeader: {
-    color: 'white',
-    fontSize: 22,
-    textAlign: 'center'
-  },
-  // infoTypeLabel: {
-  //   fontSize: 15,
-  //   textAlign: 'right',
-  //   color: 'rgba(255,0,0,0.3)',
-  //   fontFamily: 'regular',
-  //   paddingBottom: 10,
-  // },
-  // infoAnswerLabel: {
-  //   fontSize: 15,
-  //   color: 'white',
-  //   fontFamily: 'regular',
-  //   paddingBottom: 10,
-  // }
 });
