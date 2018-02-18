e96f8d7d53b12e3aeab0477940806731420163f6
tay1es <jason.thomas199@gmail.com>
2018-02-17 23:07:58 -0500

    update branch

--- components/Chat.js
+++ components/Chat.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { List, ListItem } from 'react-native-elements';
+import Search from './Search.js';
 
 
 
@@ -17,9 +18,10 @@ export default class Chat extends Component {
     return (
       <View style={styles.container}>
         <SearchBar
-          onChangeText={meme()}
-          onClearText={meme()}
+         // onChangeText={meme()}
+         // onClearText={meme()}
           placeholder='Type Here...' />
+          <Search />
       </View>
     );
   }
