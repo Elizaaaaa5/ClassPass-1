033b268b6217a98712ad0784f42fc28c1b5adbbe
tay1es <jason.thomas199@gmail.com>
2018-02-17 22:47:00 -0500

    update branch

--- components/Chat.js
+++ components/Chat.js
@@ -1,48 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import { List, ListItem } from 'react-native-elements';
 
-  const list = [
-  {
-    name: 'Amy Farha',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  }
-   // more items
-];
+
 
 
 export default class Chat extends Component {
 
-renderRow (rowData, sectionID) {
-  return (
-    <ListItem
-      avatar={<Avatar
-                rounded
-                source={rowData.avatar_url && {uri: rowData.avatar_url}}
-                title={rowData.name[0]}
-              />}
-      key={sectionID}
-      title={rowData.name}
-      subtitle={rowData.subtitle}
-    />
-  );
-}
 
-render () {
-  return (
-    <List>
-      <ListView
-        renderRow={this.renderRow}
-        dataSource={this.state.dataSource}
-      />
-    </List>
-  );
-}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SearchBar
+          onChangeText={}
+          onClearText={}
+          placeholder='Type Here...' />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
