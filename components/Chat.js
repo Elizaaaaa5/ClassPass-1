7305b076180da13de2a0da39541c38b792c79297
tay1es <jason.thomas199@gmail.com>
2018-02-17 20:14:48 -0500

    new branch

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 
-export default class Chat extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to ClassPass!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
   },
-});
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  ... // more items
+]
+
+<List containerStyle={{marginBottom: 20}}>
+  {
+    list.map((l, i) => (
+      <ListItem
+        roundAvatar
+        avatar={{uri:l.avatar_url}}
+        key={i}
+        title={l.name}
+      />
+    ))
+  }
+</List>
