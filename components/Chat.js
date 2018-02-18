832fff8732de2bcec7c940195150c3b076fa7207
tay1es <jason.thomas199@gmail.com>
2018-02-17 22:29:46 -0500

    update branch

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+
 
 export default class Chat extends Component {
   render() {
