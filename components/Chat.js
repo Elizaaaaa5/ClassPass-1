6e4e0f0c0b945a04ee9d6312ef6871684a629adf
tay1es <jason.thomas199@gmail.com>
2018-02-17 22:00:53 -0500

    new branch

--- components/Chat.js
+++ components/Chat.js
@@ -5,7 +5,7 @@ export default class Chat extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to ClassPass!</Text>
+        <Text>Welcome to Chat Tab!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
